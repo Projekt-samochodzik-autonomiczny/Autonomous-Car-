e439a8f7190e8d910b5035e5b7f105af784c79b0
Projekt-samochodzik-autonomiczny <274650@student.pwr.edu.pl>
2025-03-18 08:45:33 +0100

    data_base_car.sql

--- data_base_car.sql
+++ data_base_car.sql
@@ -1,25 +1,26 @@
 CREATE TABLE `uzytkownik` (
-  `id` integer PRIMARY KEY,
-  `imie` string,
-  `nazwisko` string,
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `imie` varchar(255),
+  `nazwisko` varchar(255),
   `indeks` integer,
   `grupa` integer
 );
 
 CREATE TABLE `pojazd` (
-  `id` integer PRIMARY KEY,
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
   `pojemnosc_baterii` float,
-  `naped` string,
+  `naped` varchar(255),
   `predkosc_obrotowa_silnika` float
 );
 
 CREATE TABLE `przejazd` (
-  `id` integer PRIMARY KEY,
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
   `czas` timestamp,
   `pojazd_id` integer NOT NULL,
-  `uzytkownik_id` integer NOT NULL,
+  `uzytkownik_id` integer NOT NULL
 );
 
 ALTER TABLE `przejazd` ADD FOREIGN KEY (`uzytkownik_id`) REFERENCES `uzytkownik` (`id`);
 
 ALTER TABLE `przejazd` ADD FOREIGN KEY (`pojazd_id`) REFERENCES `pojazd` (`id`);
+
